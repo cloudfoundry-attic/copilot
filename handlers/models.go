7197423c1b9a5cb50c3ac3266e20a3d5c8712dcf
Nick Mahoney <nmahoney@pivotal.io>
2018-03-22 16:39:11 -0700

    reorganize handlers/models.go
    [#156200800]
    
    Signed-off-by: Christian Ang <cang@pivotal.io>

--- handlers/models.go
+++ handlers/models.go
@@ -11,15 +11,29 @@ import (
 
 const CF_APP_PORT = 8080
 
-type CAPIProcessGUID string
-type DiegoProcessGUID string
-type DiegoProcessGUIDs []DiegoProcessGUID
-type Hostname string
 type RouteGUID string
+type Hostname string
+
+type Route struct {
+	GUID RouteGUID
+	Host string
+}
+
+func (r *Route) Hostname() string {
+	return r.Host
+}
 
 type RoutesRepo struct {
 	Repo map[RouteGUID]*Route
 	sync.Mutex
+}
+
+//go:generate counterfeiter -o fakes/routes_repo.go --fake-name RoutesRepo . routesRepoInterface
+type routesRepoInterface interface {
+	Upsert(route *Route)
+	Delete(guid RouteGUID)
+	Get(guid RouteGUID) (*Route, bool)
+	List() map[string]string
 }
 
 func (r *RoutesRepo) Upsert(route *Route) {
@@ -54,17 +68,27 @@ func (r *RoutesRepo) List() map[string]string {
 	return list
 }
 
-//go:generate counterfeiter -o fakes/routes_repo.go --fake-name RoutesRepo . routesRepoInterface
-type routesRepoInterface interface {
-	List() map[string]string
-	Upsert(route *Route)
-	Delete(guid RouteGUID)
-	Get(guid RouteGUID) (*Route, bool)
+type CAPIProcessGUID string
+
+type RouteMapping struct {
+	RouteGUID       RouteGUID
+	CAPIProcessGUID CAPIProcessGUID
+}
+
+func (r *RouteMapping) Key() string {
+	return string(r.RouteGUID) + "-" + string(r.CAPIProcessGUID)
 }
 
 type RouteMappingsRepo struct {
 	Repo map[string]RouteMapping
 	sync.Mutex
+}
+
+//go:generate counterfeiter -o fakes/route_mappings_repo.go --fake-name RouteMappingsRepo . routeMappingsRepoInterface
+type routeMappingsRepoInterface interface {
+	Map(routeMapping RouteMapping)
+	Unmap(routeMapping RouteMapping)
+	List() map[string]RouteMapping
 }
 
 func (m *RouteMappingsRepo) Map(routeMapping RouteMapping) {
@@ -91,16 +115,51 @@ func (m *RouteMappingsRepo) List() map[string]RouteMapping {
 	return list
 }
 
-//go:generate counterfeiter -o fakes/route_mappings_repo.go --fake-name RouteMappingsRepo . routeMappingsRepoInterface
-type routeMappingsRepoInterface interface {
-	Map(routeMapping RouteMapping)
-	Unmap(routeMapping RouteMapping)
-	List() map[string]RouteMapping
+type DiegoProcessGUID string
+
+func (p DiegoProcessGUID) InternalHostname() string {
+	label := string(p)
+
+	if len(label) >= 63 {
+		label = label[0:62]
+	}
+
+	return fmt.Sprintf("%s.cfapps.internal", label)
+}
+
+type DiegoProcessGUIDs []DiegoProcessGUID
+
+func DiegoProcessGUIDsFromStringSlice(diegoProcessGUIDs []string) DiegoProcessGUIDs {
+	diegoGUIDs := DiegoProcessGUIDs{}
+	for _, diegoGUID := range diegoProcessGUIDs {
+		diegoGUIDs = append(diegoGUIDs, DiegoProcessGUID(diegoGUID))
+	}
+	return diegoGUIDs
+}
+
+func (p DiegoProcessGUIDs) ToStringSlice() []string {
+	diegoGUIDs := []string{}
+	for _, diegoGUID := range p {
+		diegoGUIDs = append(diegoGUIDs, string(diegoGUID))
+	}
+	return diegoGUIDs
 }
 
 type CAPIDiegoProcessAssociationsRepo struct {
 	Repo map[CAPIProcessGUID]DiegoProcessGUIDs
 	sync.Mutex
+}
+
+type CAPIDiegoProcessAssociation struct {
+	CAPIProcessGUID   CAPIProcessGUID
+	DiegoProcessGUIDs DiegoProcessGUIDs
+}
+
+//go:generate counterfeiter -o fakes/capi_diego_process_associations_repo.go --fake-name CAPIDiegoProcessAssociationsRepo . capiDiegoProcessAssociationsRepoInterface
+type capiDiegoProcessAssociationsRepoInterface interface {
+	Upsert(capiDiegoProcessAssociation CAPIDiegoProcessAssociation)
+	Delete(capiProcessGUID CAPIProcessGUID)
+	List() map[string][]string
 }
 
 func (c *CAPIDiegoProcessAssociationsRepo) Upsert(capiDiegoProcessAssociation CAPIDiegoProcessAssociation) {
@@ -126,62 +185,6 @@ func (c *CAPIDiegoProcessAssociationsRepo) List() map[string][]string {
 	return list
 }
 
-//go:generate counterfeiter -o fakes/capi_diego_process_associations_repo.go --fake-name CAPIDiegoProcessAssociationsRepo . capiDiegoProcessAssociationsRepoInterface
-type capiDiegoProcessAssociationsRepoInterface interface {
-	Upsert(capiDiegoProcessAssociation CAPIDiegoProcessAssociation)
-	Delete(capiProcessGUID CAPIProcessGUID)
-	List() map[string][]string
-}
-
-func (p DiegoProcessGUID) InternalHostname() string {
-	label := string(p)
-
-	if len(label) >= 63 {
-		label = label[0:62]
-	}
-
-	return fmt.Sprintf("%s.cfapps.internal", label)
-}
-
-func DiegoProcessGUIDsFromStringSlice(diegoProcessGUIDs []string) DiegoProcessGUIDs {
-	diegoGUIDs := DiegoProcessGUIDs{}
-	for _, diegoGUID := range diegoProcessGUIDs {
-		diegoGUIDs = append(diegoGUIDs, DiegoProcessGUID(diegoGUID))
-	}
-	return diegoGUIDs
-}
-
-func (p DiegoProcessGUIDs) ToStringSlice() []string {
-	diegoGUIDs := []string{}
-	for _, diegoGUID := range p {
-		diegoGUIDs = append(diegoGUIDs, string(diegoGUID))
-	}
-	return diegoGUIDs
-}
-
 type BBSClient interface {
 	ActualLRPGroups(lager.Logger, bbsmodels.ActualLRPFilter) ([]*bbsmodels.ActualLRPGroup, error)
-}
-
-type Route struct {
-	GUID RouteGUID
-	Host string
-}
-
-func (r *Route) Hostname() string {
-	return r.Host
-}
-
-type RouteMapping struct {
-	RouteGUID       RouteGUID
-	CAPIProcessGUID CAPIProcessGUID
-}
-
-func (r *RouteMapping) Key() string {
-	return string(r.RouteGUID) + "-" + string(r.CAPIProcessGUID)
-}
-
-type CAPIDiegoProcessAssociation struct {
-	CAPIProcessGUID   CAPIProcessGUID
-	DiegoProcessGUIDs DiegoProcessGUIDs
 }
