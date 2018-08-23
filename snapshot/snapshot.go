7b1271fc52aa3d7bb4f4abab0b24420719d4339e
Zachary Gershman <zgershman@pivotal.io>
2018-08-22 20:36:57 -0700

    adds ability to make mcp snapshot in copilot

--- snapshot/snapshot.go
+++ snapshot/snapshot.go
@@ -1,42 +1,186 @@
 package snapshot
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"code.cloudfoundry.org/copilot/api"
 	"code.cloudfoundry.org/lager"
+
+	"github.com/gogo/protobuf/types"
+
+	mcp "istio.io/api/mcp/v1alpha1"
+	networking "istio.io/api/networking/v1alpha3"
+	snap "istio.io/istio/pkg/mcp/snapshot"
 )
 
-type Snapshot struct {
-	logger    lager.Logger
-	ticker    <-chan time.Time
-	collector collector
-}
+const (
+	VSTypeURL = "type.googleapis.com/istio.networking.v1alpha3.DestinationRule"
+	DRTypeURL = "type.googleapis.com/istio.networking.v1alpha3.VirtualService"
+	node      = "copilot-node-id"
+)
 
 //go:generate counterfeiter -o fakes/collector.go --fake-name Collector . collector
 type collector interface {
 	Collect() []api.RouteWithBackends
 }
 
-func NewSnapshot(logger lager.Logger, ticker <-chan time.Time, collector collector) *Snapshot {
+type builder interface {
+	Set(typeURL string, version string, resources []*mcp.Envelope)
+	Build() *snap.InMemory
+}
+
+//go:generate counterfeiter -o fakes/setter.go --fake-name Setter . setter
+type setter interface {
+	SetSnapshot(node string, istio snap.Snapshot)
+}
+
+type Snapshot struct {
+	logger    lager.Logger
+	ticker    <-chan time.Time
+	collector collector
+	builder   builder
+	setter    setter
+}
+
+func New(logger lager.Logger, ticker <-chan time.Time, collector collector, builder builder, setter setter) *Snapshot {
 	return &Snapshot{
 		logger:    logger,
 		ticker:    ticker,
 		collector: collector,
+		builder:   builder,
+		setter:    setter,
 	}
 }
 
 func (s *Snapshot) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
-	stop := make(chan struct{})
-
 	close(ready)
 
 	for {
 		select {
 		case <-signals:
-			close(stop)
 			return nil
+		case <-s.ticker:
+			routes := s.collector.Collect()
+			vsEnvelopes, drEnvelopes := s.createEnvelopes(routes)
+
+			s.builder.Set(VSTypeURL, "1", vsEnvelopes)
+			s.builder.Set(DRTypeURL, "1", drEnvelopes)
+
+			shot := s.builder.Build()
+			s.setter.SetSnapshot(node, shot)
 		}
+	}
+}
+
+func (s *Snapshot) createEnvelopes(routes []api.RouteWithBackends) ([]*mcp.Envelope, []*mcp.Envelope) {
+	var (
+		vsEnvelopes []*mcp.Envelope
+		drEnvelopes []*mcp.Envelope
+	)
+
+	httpRoutes := make(map[string]*networking.HTTPRoute)
+
+	for _, route := range routes {
+		destinationRuleName := fmt.Sprintf("copilot-rule-for-%s", route.GetHostname())
+		virtualServiceName := fmt.Sprintf("copilot-service-for-%s", route.GetHostname())
+
+		dr := createDestinationRule(&route)
+		dr.Subsets = append(dr.Subsets, createSubset(route.GetCapiProcessGuid()))
+
+		vs := createVirtualService(&route)
+
+		if r, ok := httpRoutes[route.GetHostname()+route.GetPath()]; ok {
+			r.Route = append(r.Route, createDestinationWeight(&route))
+		} else {
+			r := createHTTPRoute(&route)
+			if route.GetPath() != "" {
+				r.Match = createHTTPMatchRequest(&route)
+				vs.Http = append([]*networking.HTTPRoute{r}, vs.Http...)
+			} else {
+				vs.Http = append(vs.Http, r)
+			}
+			httpRoutes[route.GetHostname()+route.GetPath()] = r
+		}
+
+		vsResource, err := types.MarshalAny(vs)
+		if err != nil {
+			s.logger.Error("envelope.virtualservice.marshal", err) //untested
+		}
+
+		vsEnvelopes = append(vsEnvelopes, &mcp.Envelope{
+			Metadata: &mcp.Metadata{
+				Name: virtualServiceName,
+			},
+			Resource: vsResource,
+		})
+
+		drResource, err := types.MarshalAny(dr)
+		if err != nil {
+			s.logger.Error("envelope.destinationrule.marshal", err) //untested
+		}
+
+		drEnvelopes = append(drEnvelopes, &mcp.Envelope{
+			Metadata: &mcp.Metadata{
+				Name: destinationRuleName,
+			},
+			Resource: drResource,
+		})
+	}
+
+	return vsEnvelopes, drEnvelopes
+}
+
+func createVirtualService(route *api.RouteWithBackends) *networking.VirtualService {
+	return &networking.VirtualService{
+		Gateways: []string{"cloudfoundry-ingress"},
+		Hosts:    []string{route.GetHostname()},
+	}
+}
+
+func createDestinationWeight(route *api.RouteWithBackends) *networking.DestinationWeight {
+	return &networking.DestinationWeight{
+		Destination: &networking.Destination{
+			Host:   route.GetHostname(),
+			Subset: route.GetCapiProcessGuid(),
+			Port: &networking.PortSelector{
+				Port: &networking.PortSelector_Number{
+					Number: 8080,
+				},
+			},
+		},
+		Weight: route.GetRouteWeight(),
+	}
+}
+
+func createHTTPRoute(route *api.RouteWithBackends) *networking.HTTPRoute {
+	return &networking.HTTPRoute{
+		Route: []*networking.DestinationWeight{createDestinationWeight(route)},
+	}
+}
+
+func createHTTPMatchRequest(route *api.RouteWithBackends) []*networking.HTTPMatchRequest {
+	return []*networking.HTTPMatchRequest{
+		{
+			Uri: &networking.StringMatch{
+				MatchType: &networking.StringMatch_Prefix{
+					Prefix: route.GetPath(),
+				},
+			},
+		},
+	}
+}
+
+func createSubset(capiProcessGUID string) *networking.Subset {
+	return &networking.Subset{
+		Name:   capiProcessGUID,
+		Labels: map[string]string{"cfapp": capiProcessGUID},
+	}
+}
+
+func createDestinationRule(route *api.RouteWithBackends) *networking.DestinationRule {
+	return &networking.DestinationRule{
+		Host: route.GetHostname(),
 	}
 }
--- snapshot/snapshot_test.go
+++ snapshot/snapshot_test.go
@@ -1,1 +1,126 @@
 package snapshot_test
+
+import (
+	"os"
+	"time"
+
+	"code.cloudfoundry.org/copilot/api"
+	"code.cloudfoundry.org/copilot/snapshot"
+	"code.cloudfoundry.org/copilot/snapshot/fakes"
+	"code.cloudfoundry.org/lager/lagertest"
+
+	networking "istio.io/api/networking/v1alpha3"
+	snap "istio.io/istio/pkg/mcp/snapshot"
+
+	"github.com/gogo/protobuf/types"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Run", func() {
+	var (
+		ticker    chan time.Time
+		s         *snapshot.Snapshot
+		collector *fakes.Collector
+		setter    *fakes.Setter
+	)
+
+	BeforeEach(func() {
+		l := lagertest.TestLogger{}
+		ticker = make(chan time.Time)
+		collector = &fakes.Collector{}
+		setter = &fakes.Setter{}
+		builder := snap.NewInMemoryBuilder()
+
+		s = snapshot.New(l, ticker, collector, builder, setter)
+	})
+
+	It("sends mcp snapshots", func() {
+		sig := make(chan os.Signal)
+		ready := make(chan struct{})
+
+		collector.CollectReturns([]api.RouteWithBackends{
+			{
+				Hostname: "foo.example.com",
+				Path:     "/something",
+				Backends: &api.BackendSet{
+					Backends: []*api.Backend{
+						{
+							Address: "10.10.10.1",
+							Port:    uint32(65003),
+						},
+					},
+				},
+				CapiProcessGuid: "a-capi-guid",
+				RouteWeight:     int32(100),
+			},
+		})
+
+		go s.Run(sig, ready)
+		ticker <- time.Time{}
+
+		Eventually(setter.SetSnapshotCallCount).Should(Equal(1))
+		node, shot := setter.SetSnapshotArgsForCall(0)
+		Expect(node).To(Equal("copilot-node-id"))
+
+		virtualServices := shot.Resources(snapshot.VSTypeURL)
+		destinationRules := shot.Resources(snapshot.DRTypeURL)
+
+		Expect(virtualServices).To(HaveLen(1))
+		Expect(virtualServices[0].Metadata.Name).To(Equal("copilot-service-for-foo.example.com"))
+
+		Expect(destinationRules).To(HaveLen(1))
+		Expect(destinationRules[0].Metadata.Name).To(Equal("copilot-rule-for-foo.example.com"))
+
+		var vs networking.VirtualService
+		err := types.UnmarshalAny(virtualServices[0].Resource, &vs)
+		Expect(err).NotTo(HaveOccurred())
+
+		var dr networking.DestinationRule
+		err = types.UnmarshalAny(destinationRules[0].Resource, &dr)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(vs).To(Equal(networking.VirtualService{
+			Hosts:    []string{"foo.example.com"},
+			Gateways: []string{"cloudfoundry-ingress"},
+			Http: []*networking.HTTPRoute{
+				{
+					Match: []*networking.HTTPMatchRequest{
+						{
+							Uri: &networking.StringMatch{
+								MatchType: &networking.StringMatch_Prefix{
+									Prefix: "/something",
+								},
+							},
+						},
+					},
+					Route: []*networking.DestinationWeight{
+						{
+							Destination: &networking.Destination{
+								Host: "foo.example.com",
+								Port: &networking.PortSelector{
+									Port: &networking.PortSelector_Number{
+										Number: 8080,
+									},
+								},
+								Subset: "a-capi-guid",
+							},
+							Weight: 100,
+						},
+					},
+				},
+			},
+		}))
+		Expect(dr).To(Equal(networking.DestinationRule{
+			Host: "foo.example.com",
+			Subsets: []*networking.Subset{
+				{
+					Name:   "a-capi-guid",
+					Labels: map[string]string{"cfapp": "a-capi-guid"},
+				},
+			},
+		}))
+
+		sig <- os.Kill
+	})
+})
